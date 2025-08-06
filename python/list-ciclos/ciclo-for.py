7d7d22612ba14bf2abc519ddb019ac18f6e7b204
Romario Rodriguez <romariorodriguez17@gmail.com>
2025-08-06 11:37:02 -0500

    (feat(while-for) Se añaden ejemplos de for y while

---
+++ python/list-ciclos/ciclo-for.py
@@ -0,0 +1,29 @@
+# Ejemplo 1: Iterando a través de una lista de números
+numbers = [1, 2, 3, 4, 5]
+for num in numbers:
+  print(f"El número actual es: {num}")
+
+# Ejemplo 2: Usando range() para crear una secuencia
+# Esto imprimirá números del 0 al 4
+for i in range(5):
+  print(f"Iteración {i}")
+
+# Ejemplo 3: Iterando a través de una cadena de texto
+message = "Hello"
+for letter in message:
+  print(f"Letra: {letter}")
+
+## ciclo for otros ejemplos 
+
+frutas = ["manzana", "banana", "cereza", "naranja"]
+for fruta in frutas:
+    print(f"Me gusta la {fruta}.")
+
+##Iterando sobre una cadena de texto
+palabra = "Python"
+for letra in palabra:
+    print(letra)
+
+ # Recorre los números del 0 al 4 (sin incluir el 5)
+for i in range(5):
+ print(f"Iteración número: {i}")
---
+++ python/list-ciclos/ciclo-while.py
@@ -0,0 +1,15 @@
+sumatoria =0
+numero = 1 
+
+while numero <= 10:
+  sumatoria = sumatoria + numero
+  numero += 1
+print(f"La sumatoria de los números del 1 al 10 es: {sumatoria}")
+
+
+
+
+numero=0
+while numero <=10:
+  print(f"El número actual es: {numero}")
+  numero += 1
